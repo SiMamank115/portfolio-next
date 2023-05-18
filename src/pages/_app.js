53ce28ed3dc8a14c54cdeb88ea171616ad6c86f3
SiMamank115 <faeest.115@gmail.com>
2023-05-18 19:57:10 +0700

    hero text animation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,19 @@
 import Loading from "@/components/loading";
 import LoadImage from "@/components/preload";
+import "@/styles/fonts.css";
 import "@/styles/globals.css";
 import "aos/dist/aos.css";
+import { Quicksand } from "next/font/google";
 import { useEffect, useState } from "react";
 
+const quicksand = Quicksand({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
     const [load, setLoad] = useState(false);
     const [imageloaded, setLmageloaded] = useState(false);
     useEffect(() => {
         window.scrollTo(0, 0);
         async function fetchData() {
-            await LoadImage(["https://i.imgur.com/nal1uUk.jpg", "lifestyle-night-city.jpg"]);
+            await LoadImage(["lifestyle-night-city.jpg"]);
             setLmageloaded(true);
         }
         if (imageloaded) {
@@ -20,9 +23,9 @@ export default function App({ Component, pageProps }) {
         }
     }, [imageloaded]);
     return (
-        <>
+        <main className={quicksand.className}>
             <Component {...pageProps} />
             <Loading loaded={load} />
-        </>
+        </main>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,34 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter, Merriweather } from "next/font/google";
 import Toggler from "@/components/ModeToggler";
-import Loading from "@/components/loading";
-
-const inter = Inter({ subsets: ["latin"] });
-
+import { useEffect, useState } from "react";
 export default function Home() {
-    return (
-		<div>
-			<img src="lifestyle-night-city.jpg" className="h-[90vh]"></img>
-			<div className="text-9xl flex">
-				<div className="block m-0 p-0" data-aos="fade-up">L</div>
-				<div className="block m-0 p-0" data-aos="fade-up" data-aos-delay="100">O</div>
-				<div className="block m-0 p-0" data-aos="fade-up" data-aos-delay="200">L</div>
-			</div>
-		</div>
-	);
+    const animation = ["fade-up", "fade-down", "fade-right", "fade-left", "fade-up-right", "fade-up-left", "fade-down-right", "fade-down-left", "zoom-in-up", "zoom-in-down", "zoom-in-left", "zoom-in-right", "zoom-out-up", "zoom-out-down", "zoom-out-right", "zoom-out-left"],
+        hero_text = "faiz ramadhan",
+        [aos_hooks, set_aos_hooks] = useState({}),
+        [text_animation, set_text_animation] = useState([]),
+        [animated, setAnimated] = useState(false);
+    useEffect(() => {
+        if (!animated) setAnimated(true);
+        set_text_animation(
+            hero_text.toUpperCase().split("").map((e, x) => {
+                let keyname = "text-anim-" + (e == " " ? "space" : e) + "-" + x;
+                set_aos_hooks((previousState) => {
+                    let res = { ...previousState };
+                    res[keyname] = animation[Math.floor(Math.random() * animation.length)];
+                    return res;
+                });
+                return (
+                    <div data-aos-delay={50 * x} key={keyname} className={"block my-0 -mx-[1px] " + (e == " " ? "px-[1%]" : "p-0")} data-aos-easing="ease-out-back" data-aos={aos_hooks[keyname]}>
+                        {e}
+                    </div>
+                );
+            })
+        );
+        if (text_animation.length != 0) return;
+    }, [animated]);
+    return <div className="text-3xl sm:text-6xl flex min-h-screen flex-wrap justify-center items-center font-bold">{text_animation}</div>;
 }
+// fade-up		 fade-down		 fade-right		 fade-left
+// fade-up-right fade-up-left	 fade-down-right fade-down-left
+// zoom-in-up	 zoom-in-down	 zoom-in-left	 zoom-in-right
+// zoom-out-up	 zoom-out-down	 zoom-out-right	 zoom-out-left
