80ae1ef289785e51745739bb554ab400429faf25
SiMamank115 <faeest.115@gmail.com>
2023-05-21 21:17:32 +0700

    parallax again

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
     useEffect(() => {
         window.scrollTo(0, 0);
         async function fetchData() {
-            await LoadImage(["layer-1-long.webp"]);
+            await LoadImage(["layer-1.webp","layer-2.webp"]);
             setLmageloaded(true);
         }
         if (imageloaded) {
